11e241b49fc9e6f3c39d12ef2c12b32644ce6aee
jjchern <jiajia.chern@gmail.com>
2018-09-08 19:14:56 -0500

    Identifiers should be strings

--- data-raw/prepare.R
+++ data-raw/prepare.R
@@ -1,4 +1,6 @@
 
+library(tidyverse)
+
 # Download crosswalk file -------------------------------------------------
 # Download date: "2015-11-07 23:43:08 CST"
 
@@ -6,20 +8,12 @@ url = "http://www.udsmapper.org/docs/zip_to_zcta_2015.xlsx"
 lcl = "data-raw/zip_to_zcta_2015.xlsx"
 if(!file.exists(lcl)) download.file(url, lcl)
 
-# Read the data -----------------------------------------------------------
+# Minimum processing needed -----------------------------------------------
 
-zipzcta = readxl::read_excel(lcl)
-
-# Variable names -----------------------------------------------
-
-library(dplyr)
-library(magrittr)
-names(zipzcta) = zipzcta %>% names %>% tolower()
-zipzcta %<>%
-  mutate(zip = as.integer(zip),
-         zcta = as.integer(zcta))
+readxl::read_excel(lcl) %>%
+  docxtractr::mcga() %>%
+  print() -> zipzcta
 
 # Save --------------------------------------------------------------------
 
 devtools::use_data(zipzcta, overwrite = TRUE)
-
